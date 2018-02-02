1286b8666b66ef7b97af1ce7f7cea6a1ec205691
root <root@BoopoBeep.localdomain>
2018-02-02 18:31:38 -0500

    Changed role to use wmi

--- roles/dns-deploy/files/Add-DnsRecords.ps1
+++ roles/dns-deploy/files/Add-DnsRecords.ps1
@@ -3,6 +3,7 @@
 param (
     [Parameter(Mandatory=$true)][string]$FileName,
     [Parameter(Mandatory=$true)][int32]$TeamNumber,
+    [Parameter(Mandatory=$true)][string]$server,
     [switch]$CreatePtrs = $true
 )
 
@@ -18,22 +19,39 @@ Import-Csv $FileName | % {
         Add-DnsServerPrimaryZone -NetworkId "$NetworkId" -ReplicationScope Domain
         $revzones += $RevZoneName
     }
-
+    
+    
     $fqdn = $_.Name.replace("teamX","team$TeamNumber")
     $name = $fqdn.split(".")[0]
     $domain_split = $fqdn.split(".")
     $domain = ($domain_split[1..($domain_split.length -1)] -join ".")
+    
+    $alias = $_.Address
+    $priority = "10"
+    $srvr = $server -split "\." 
+    $addr = $ip -split "\."
+    $rec = [WmiClass]"\\$($srvr[0])\root\MicrosoftDNS:MicrosoftDNS_ResourceRecord" 
+    
+
     if ($_.Type -eq "A"){
-        Add-DnsServerResourceRecordA -Name $name -IPv4Address $ip -ZoneName $domain -CreatePtr:$CreatePtrs
+        #Add-DnsServerResourceRecordA -Name $name -IPv4Address $ip -ZoneName $domain -CreatePtr:$CreatePtrs
+        $text = "$fqdn IN A $ip"  
+        $rec.CreateInstanceFromTextRepresentation($server, $domain, $text)  
     }
     if ($_.Type -eq "MX"){
-        Add-DnsServerResourceRecordMX -Name . -Preference 10 -MailExchange $fqdn -ZoneName $domain
+        #Add-DnsServerResourceRecordMX -Name . -Preference 10 -MailExchange $fqdn -ZoneName $domain
+        $text = "$domain IN MX $priority $fqdn"  
+        $rec.CreateInstanceFromTextRepresentation($server, $domain, $text) 
     }
     if ($_.Type -eq "CNAME"){
-        Add-DnsServerResourceRecordCName -Name $name -HostNameAlias $_.Address -ZoneName $domain
+        #Add-DnsServerResourceRecordCName -Name $name -HostNameAlias $_.Address -ZoneName $domain
+        $text = "$alias IN CNAME $fqdn"  
+        $rec.CreateInstanceFromTextRepresentation($server, $domain, $text)
     }
     if ($CreatePtrs -eq $true) {
-        Add-DnsServerResourceRecordPtr -name $_.Address.split(".")[3] -ZoneName $RevZoneName -PtrDomainName $fqdn -ErrorAction SilentlyContinue
+        #Add-DnsServerResourceRecordPtr -name $_.Address.split(".")[3] -ZoneName $RevZoneName -PtrDomainName $fqdn -ErrorAction SilentlyContinue
+        $text = "$($addr[3]).$RevZoneName IN PTR $fqdn"  
+        $rec.CreateInstanceFromTextRepresentation($server, $RevZoneName, $text)
     }
 }
 

410d9dbd9cf08c141fddc0f5d9c211765ee043bd
Kyle Carretto <kcarretto@gmail.com>
2018-02-03 02:20:20 -0500

    Good to deploy? Missing windows domain users, bsd users, and not sure if domain join works (linux)

---
+++ etchosts.py
@@ -0,0 +1,11 @@
+
+for i in range(0, 12):
+	print("10.2.{}.10 vega.team{}.ists".format(i,i))
+	print("10.2.{}.20 sirius.team{}.ists".format(i,i))
+	print("10.2.{}.30 tauri.team{}.ists".format(i,i))
+	print("10.2.{}.40 sol.team{}.ists".format(i,i))
+	print("10.2.{}.50 crucis.team{}.ists".format(i,i))
+	print("10.2.{}.60 persei.team{}.ists".format(i,i))
+	print("10.3.{}.10 pegasi.team{}.ists".format(i,i))
+	print("10.3.{}.20 wolf.team{}.ists".format(i,i))
+
